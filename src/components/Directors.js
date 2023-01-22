85414e66797980ed022d3e0636244af9c49b8e61
Preston <prestonosoro56@gmail.com>
2023-01-22 20:54:24 +0300

    Render Text amd make new Div..

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  // make a new div for each director then map data
+  const director = directors.map((director) => (
+    <div key={director.name}>
+      <h2>{director.name}</h2>
+      <ul>
+        {director.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+  return (
+    <div>
+      <h1>Directors Page </h1>
+      {director}
+    </div>
+  );
 }
 
 export default Directors;
