b2783c9eb1d9d18c6333ca0a2c812b1d5f4f631d
Preston <prestonosoro56@gmail.com>
2023-01-22 20:48:53 +0300

    Refactor movies

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  // map method to loop through a set of data
+  const myMovies = movies.map((movie) => (
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <p>Runtime: {movie.time} min.</p>
+
+      <ul>
+        {movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {myMovies}
+    </div>
+  );
 }
 
 export default Movies;
